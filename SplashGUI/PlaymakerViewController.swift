9757c9785157f323c7e6f05f3cdd999350747ef9
Omer Hyjaneh <hyajneh.1@osu.edu>
2023-06-06 09:42:01 -0400

    New UI for playmaker tab - Removed feedbackTextView - Added scroll view containing a text editor for modifying .md files - Added buttons below playgroundTextEditor that generate markdown elements - Added a save button (not implemented yet) - Added fileList stack view in a scroll view, the buttons within will open the file in the text editor (not implemented yet)

--- SplashGUI/PlaymakerViewController.swift
+++ SplashGUI/PlaymakerViewController.swift
@@ -9,48 +9,94 @@ import UIKit
 import CoreServices
 import playmaker
 
+//fileNames
+var fileURLs: [URL] = []
+
 class PlaymakerViewController: UIViewController, UIDocumentPickerDelegate {
 
     var pickedFolderURL: URL?
     var directoryContainsMDFiles: Bool = false
+    //folder
+    var folderDirectory: URL? = nil
+    
+    
+    
     
     @IBOutlet var playgroundIt: UIButton!
-    @IBOutlet var feedbackTextView: UITextView!
+    @IBOutlet var playgroundTextEditor: UITextView!
+    @IBOutlet var fileList: UIStackView!
     
+    //File Buttons
+    @IBOutlet var nextFileButton: UIButton!
+    @IBOutlet var saveButton: UIButton!
+    
+    // Text Editor Buttons
+    @IBOutlet var h1Button: UIButton!
+    @IBOutlet var h2Button: UIButton!
+    @IBOutlet var h3Button: UIButton!
+    @IBOutlet var noteButton: UIButton!
+    @IBOutlet var codeBlockButton: UIButton!
+    @IBOutlet var codeLineButton: UIButton!
+    @IBOutlet var boldButton: UIButton!
+    @IBOutlet var italicsButton: UIButton!
+    @IBOutlet var ulButton: UIButton!
+    @IBOutlet var horizontalButton: UIButton!
+    @IBOutlet var linkButton: UIButton!
+    @IBOutlet var imageButton: UIButton!
+    
+    /* -------------------- Functions Below ------------------*/
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.feedbackTextView.text = ""
         self.playgroundIt.isEnabled = false
+        self.playgroundTextEditor.text = ""
     }
     
+
     func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
         guard let safelyWrappedURL = urls.first else { return }
         self.pickedFolderURL = safelyWrappedURL
         
-        self.feedbackTextView.text = "Input ⟶ \(safelyWrappedURL.relativePath )\n\n"
-        let mdFiles = safelyWrappedURL.getFilesOfTypeInDirectory(directory: safelyWrappedURL, fileExtension: "md")
+        clearFileList()
         
-        mdFiles.forEach { mdFileStr in
-            self.feedbackTextView.text = self.feedbackTextView.text + "\n" + mdFileStr
-
-        }
+        let mdFiles = safelyWrappedURL.getFilesOfTypeInDirectory(directory: safelyWrappedURL, fileExtension: "md")
         
         if mdFiles.count > 0 {
             directoryContainsMDFiles = true
             self.playgroundIt.isEnabled = true
-        } else {
-            self.feedbackTextView.text = "Input ⟶ \(safelyWrappedURL.relativePath )\n\nNo `.MD` files were found!"
         }
+        
+        setupFileList()
+        
     }
     
-    @IBAction func selectFile(_ sender: Any) {
+    
+    @IBAction func selectFolder(_ sender: Any) {
+        
         let documentPickerController = UIDocumentPickerViewController(documentTypes: [kUTTypeFolder as String], in: .open)
         
         documentPickerController.delegate = self
         
-        self.present(documentPickerController, animated: true)
+        present(documentPickerController, animated: true)
+        
     }
     
+    
+    @IBAction func saveButton(_ sender: Any) {
+        guard let folderDirectory = folderDirectory else {
+                // Handle the case where fileDirectory is nil or not set
+                return
+        }
+            
+        let textToSave = playgroundTextEditor.text ?? ""
+        let fileURL = folderDirectory.appendingPathComponent(fileURLs[0].description)
+        
+        do {
+            try textToSave.write(to: fileURL, atomically: true, encoding: .utf8)
+            print("File saved successfully at \(fileURL.path)")
+        } catch {
+            print("Error saving file: \(error.localizedDescription)")
+        }
+    }
     
     
     @IBAction func playgroundize(_ sender: Any) {
@@ -63,17 +109,275 @@ class PlaymakerViewController: UIViewController, UIDocumentPickerDelegate {
         let file = safelyUnwrapped.getFilesOfTypeInDirectory(directory: safelyUnwrapped, fileExtension: "playground")
         
         if(file.count > 0) {
-            self.feedbackTextView.text = "Output ⟶ " + file[0]
             cleanUp()
         }
-        
     }
-    
+        
     func cleanUp() {
         self.pickedFolderURL = nil
         self.playgroundIt.isEnabled = false
     }
     
+    
+    @IBAction func h1ButtonTapped(_ sender: Any) {
+        // Get the current cursor position
+        let cursorPosition = playgroundTextEditor.selectedRange.location
+        
+        // Get the current text of the UITextView
+        var newText = playgroundTextEditor.text ?? ""
+            
+        // Insert the opening quote mark behind the cursor position
+        newText.insert("#", at: newText.index(newText.startIndex, offsetBy: cursorPosition))
+            
+        // Update the text of the UITextView
+        playgroundTextEditor.text = newText
+            
+        // Adjust the cursor position after the inserted symbol
+        playgroundTextEditor.selectedRange = NSMakeRange(cursorPosition + 1, 0)
+    }
+    
+    
+    @IBAction func h2ButtonTapped(_ sender: Any) {
+        // Get the current cursor position
+        let cursorPosition = playgroundTextEditor.selectedRange.location
+        
+        // Get the current text of the UITextView
+        var newText = playgroundTextEditor.text ?? ""
+            
+        // Insert the opening quote mark behind the cursor position
+        newText.insert(contentsOf:"##", at: newText.index(newText.startIndex, offsetBy: cursorPosition))
+            
+        // Update the text of the UITextView
+        playgroundTextEditor.text = newText
+            
+        // Adjust the cursor position after the inserted symbol
+        playgroundTextEditor.selectedRange = NSMakeRange(cursorPosition + 2, 0)
+    }
+    
+    
+    @IBAction func h3ButtonTapped(_ sender: Any) {
+        // Get the current cursor position
+        let cursorPosition = playgroundTextEditor.selectedRange.location
+        
+        // Get the current text of the UITextView
+        var newText = playgroundTextEditor.text ?? ""
+            
+        // Insert the opening quote mark behind the cursor position
+        newText.insert(contentsOf:"###", at: newText.index(newText.startIndex, offsetBy: cursorPosition))
+            
+        // Update the text of the UITextView
+        playgroundTextEditor.text = newText
+            
+        // Adjust the cursor position after the inserted symbol
+        playgroundTextEditor.selectedRange = NSMakeRange(cursorPosition + 3, 0)
+    }
+    
+    
+    @IBAction func noteButtonTapped(_ sender: Any) {
+        // Get the current cursor position
+        let cursorPosition = playgroundTextEditor.selectedRange.location
+        
+        // Get the current text of the UITextView
+        var newText = playgroundTextEditor.text ?? ""
+            
+        // Insert the opening quote mark behind the cursor position
+        newText.insert(">", at: newText.index(newText.startIndex, offsetBy: cursorPosition))
+            
+        // Update the text of the UITextView
+        playgroundTextEditor.text = newText
+            
+        // Adjust the cursor position after the inserted quote mark
+        playgroundTextEditor.selectedRange = NSMakeRange(cursorPosition + 1, 0)
+    }
+    
+    
+    @IBAction func codeBlockButtonTapped(_ sender: Any) {
+        // Get the current cursor position
+        let cursorPosition = playgroundTextEditor.selectedRange.location
+        
+        // Get the current text of the UITextView
+        var newText = playgroundTextEditor.text ?? ""
+            
+        // Insert the opening quote mark behind the cursor position
+        newText.insert(contentsOf:"```\n\n```", at: newText.index(newText.startIndex, offsetBy: cursorPosition))
+            
+        // Update the text of the UITextView
+        playgroundTextEditor.text = newText
+            
+        // Adjust the cursor position after the inserted quote mark
+        playgroundTextEditor.selectedRange = NSMakeRange(cursorPosition + 4, 0)
+    }
+    
+    
+    @IBAction func codeLineButtonTapped(_ sender: Any) {
+        // Get the current cursor position
+        let cursorPosition = playgroundTextEditor.selectedRange.location
+        
+        // Get the current text of the UITextView
+        var newText = playgroundTextEditor.text ?? ""
+            
+        // Insert the opening quote mark behind the cursor position
+        newText.insert(contentsOf:"``", at: newText.index(newText.startIndex, offsetBy: cursorPosition))
+            
+        // Update the text of the UITextView
+        playgroundTextEditor.text = newText
+            
+        // Adjust the cursor position after the inserted quote mark
+        playgroundTextEditor.selectedRange = NSMakeRange(cursorPosition + 1, 0)
+    }
+    
+    
+    @IBAction func boldButtonTapped(_ sender: Any) {
+        // Get the current cursor position
+        let cursorPosition = playgroundTextEditor.selectedRange.location
+        
+        // Get the current text of the UITextView
+        var newText = playgroundTextEditor.text ?? ""
+            
+        // Insert the opening quote mark behind the cursor position
+        newText.insert(contentsOf:"****", at: newText.index(newText.startIndex, offsetBy: cursorPosition))
+            
+        // Update the text of the UITextView
+        playgroundTextEditor.text = newText
+            
+        // Adjust the cursor position after the inserted quote mark
+        playgroundTextEditor.selectedRange = NSMakeRange(cursorPosition + 2, 0)
+    }
+    
+    
+    @IBAction func italicsButtonTapped(_ sender: Any) {
+        // Get the current cursor position
+        let cursorPosition = playgroundTextEditor.selectedRange.location
+        
+        // Get the current text of the UITextView
+        var newText = playgroundTextEditor.text ?? ""
+            
+        // Insert the opening quote mark behind the cursor position
+        newText.insert(contentsOf:"**", at: newText.index(newText.startIndex, offsetBy: cursorPosition))
+            
+        // Update the text of the UITextView
+        playgroundTextEditor.text = newText
+            
+        // Adjust the cursor position after the inserted quote mark
+        playgroundTextEditor.selectedRange = NSMakeRange(cursorPosition + 1, 0)
+    }
+    
+    
+    @IBAction func ulButtonTapped(_ sender: Any) {
+        // Get the current cursor position
+        let cursorPosition = playgroundTextEditor.selectedRange.location
+        
+        // Get the current text of the UITextView
+        var newText = playgroundTextEditor.text ?? ""
+            
+        // Insert the opening quote mark behind the cursor position
+        newText.insert(contentsOf:"* ", at: newText.index(newText.startIndex, offsetBy: cursorPosition))
+            
+        // Update the text of the UITextView
+        playgroundTextEditor.text = newText
+            
+        // Adjust the cursor position after the inserted quote mark
+        playgroundTextEditor.selectedRange = NSMakeRange(cursorPosition + 2, 0)
+    }
+    
+    
+    @IBAction func horizontalButtonTapped(_ sender: Any) {
+        // Get the current cursor position
+        let cursorPosition = playgroundTextEditor.selectedRange.location
+        
+        // Get the current text of the UITextView
+        var newText = playgroundTextEditor.text ?? ""
+            
+        // Insert the opening quote mark behind the cursor position
+        newText.insert(contentsOf:"---\n", at: newText.index(newText.startIndex, offsetBy: cursorPosition))
+            
+        // Update the text of the UITextView
+        playgroundTextEditor.text = newText
+            
+        // Adjust the cursor position after the inserted quote mark
+        playgroundTextEditor.selectedRange = NSMakeRange(cursorPosition + 4, 0)
+    }
+    
+    
+    @IBAction func linkButtonTapped(_ sender: Any) {
+        // Get the current cursor position
+        let cursorPosition = playgroundTextEditor.selectedRange.location
+        
+        // Get the current text of the UITextView
+        var newText = playgroundTextEditor.text ?? ""
+            
+        // Insert the opening quote mark behind the cursor position
+        newText.insert(contentsOf:"[<insert title here>](<insert URL here>)\n", at: newText.index(newText.startIndex, offsetBy: cursorPosition))
+            
+        // Update the text of the UITextView
+        playgroundTextEditor.text = newText
+            
+        // Adjust the cursor position after the inserted quote mark
+        playgroundTextEditor.selectedRange = NSMakeRange(cursorPosition + 1, 0)
+    }
+    
+    
+    @IBAction func imageButtonTapped(_ sender: Any) {
+        // Get the current cursor position
+        let cursorPosition = playgroundTextEditor.selectedRange.location
+        
+        // Get the current text of the UITextView
+        var newText = playgroundTextEditor.text ?? ""
+            
+        // Insert the opening quote mark behind the cursor position
+        newText.insert(contentsOf:"![<alt text>](<image.jpg>)", at: newText.index(newText.startIndex, offsetBy: cursorPosition))
+            
+        // Update the text of the UITextView
+        playgroundTextEditor.text = newText
+            
+        // Adjust the cursor position after the inserted quote mark
+        playgroundTextEditor.selectedRange = NSMakeRange(cursorPosition + 2, 0)
+    }
+    
+    
+    func createButton(name: String) {
+        // Create a new button object
+        let fileButton = UIButton(type: .roundedRect)
+        // Set the button's properties.
+        playgroundTextEditor.text.append("\(name)\n")
+        // Set its background color
+        fileButton.backgroundColor = UIColor.systemBlue
+
+        // Set its title color
+        fileButton.setTitleColor(UIColor.white, for: .normal)
+
+        // Set its title
+        fileButton.setTitle(name, for: .normal)
+        fileButton.contentHorizontalAlignment = .left
+        fileButton.titleEdgeInsets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0)
+
+        // Add the button to the view hierarchy.
+        fileList.addArrangedSubview(fileButton)
+        
+    }
+    
+    func setupFileList() {
+        /*
+         * Re-implement list using a vertical stack of buttons that need to be coded instead of dragged/dropped
+         */
+        sortFileList()
+        for currentFile in fileURLs {
+            createButton(name: currentFile.lastPathComponent.description)
+        }
+    }
+    
+    func clearFileList(){
+        fileURLs = []
+        for fileButton in fileList.arrangedSubviews {
+            fileList.removeArrangedSubview(fileButton)
+            fileButton.removeFromSuperview()
+        }
+    }
+    
+    func sortFileList(){
+        fileURLs = fileURLs.sorted { $0.lastPathComponent.lowercased() < $1.lastPathComponent.lowercased() }
+    }
+
 }
 
 extension URL {
@@ -87,6 +391,7 @@ extension URL {
                     let fileAttributes = try fileURL.resourceValues(forKeys:[.isRegularFileKey])
                     if fileURL.pathExtension == fileExtension {
                         stringDisplay.append("\t\(fileURL.relativePath)\n")
+                        fileURLs.append(fileURL)
                     }
                 } catch { print(error, fileURL) }
             }
